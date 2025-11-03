586b4f246f81beaf56b5f8a82dc4de0935915a3f
Michael Lawlor <michaell16996@gmail.com>
2025-11-02 20:50:04 -0700

    added basic game logic

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,5 +1,6 @@
 import random
 
+#Creating necessary initial variables
 list = ['quality', 'dull', 'cherry', 'symbol', 'angel', 'evening', 'authorise', 'chemistry', 'marriage', 'disturbance']
 choice = random.choice(list)
 guess = ''
@@ -9,4 +10,33 @@ for letter in choice:
     guess = guess + '*'
 
 print('Guess the word one letter at a time!')
-# Write Logic
+
+# Writing game logic
+while guess != choice:
+
+    print(f'Remaining attempts: {attempts}')
+    print(f'{guess}\n')
+    letter_choice = input('Enter a letter: ').lower()
+
+    if len(letter_choice) == 1:
+
+        if letter_choice in choice:
+            new_guess = ''
+            counter = 0
+            for letter in choice:
+                if choice[counter] == letter_choice:
+                    new_guess = new_guess + letter_choice
+                else:
+                    new_guess = new_guess + guess[counter]
+                counter = counter + 1
+            guess = new_guess
+
+        else:
+            print(f'The word does not contain the letter: {letter_choice}\n')
+            attempts = attempts - 1
+
+    else:
+        print('Please enter one letter guesses only.\n')
+
+
+
